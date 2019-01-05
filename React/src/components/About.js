1f1a9b3904c0c1663a51f664648ea45913ddef41
Harnick Khera <Hephyrius@gmail.com>
2019-01-05 07:20:30 +0000

    updated about

--- React/src/components/About.js
+++ React/src/components/About.js
@@ -11,30 +11,11 @@ class About extends React.Component {
         <div class="container">
             <div class="row">
             <form onSubmit={this.handleSubmit}>
-            <h3>About !Filter</h3>
+            <h3>About TAiL</h3>
                 <div>
                 <p align="justify">
-                NoFilter is a dApp media platform. The platform allows users to share content with other users, akin to platforms such as medium or reddit. 
-                It leverages the TronVM event system as a means of storing content in an immutable fashion. The platform allows users to reward the content 
-                they like with donations (in trx) and gilding (with the native platform token). The dApp also makes use of Tron SmartContracts to keep 
-                track of statistics such as Votes and Donations. The fee model of the dApp is that, a small portion of gildings and donations are psythoned 
-                off into a development fund which can support future upgrades and maintenence of the dApp.
-                </p>
-
-                <p align="justify">
-                The dApp comes in two flavours, Online and Web3. 
-                </p>
-
-                <p align="justify">
-                The Web3 version is the unadulterated version of the dApp. This version is a desktop (electon) app that allows the user to tap directly 
-                into a TVM fullhost/api of their choice. The Web3 flavour uses the events system and smart contract as a database, whilst also saving 
-                content to the local machine, when needed/required. Due to the fact that there is no intermediatary, this flavour is entirely uncensored.
-                </p>
-
-                <p align="justify">
-                The online version of the dapp uses TronLink & trongrid apis in order to interact with the smart contract. As well as a server and DB that 
-                stores the events and smart contract data so that it can be accessed easily, without overwhelming the trongrid api platform. This version 
-                of the dApp may lead to censorship due to the database/server that is between the dApp and Blockchain.
+                TAiL Is an AI Library and Deployer. The Library is used to train a neural network using python and user defined data. The Deployer is the front end of the dApp responsible for creating the network on the TVM and interacting with it. Iteraction being prediction on new data.
+                The Frontend also allows Historic predictions to be reviewed.
                 </p>
 
                 </div>
