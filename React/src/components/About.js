4aeede7699f874b5b7959e0e8534927d694acdd5
Harnick Khera <Hephyrius@gmail.com>
2019-01-05 07:21:45 +0000

    Update About.js

--- React/src/components/About.js
+++ React/src/components/About.js
@@ -16,6 +16,8 @@ class About extends React.Component {
                 <p align="justify">
                 TAiL Is an AI Library and Deployer. The Library is used to train a neural network using python and user defined data. The Deployer is the front end of the dApp responsible for creating the network on the TVM and interacting with it. Iteraction being prediction on new data.
                 The Frontend also allows Historic predictions to be reviewed.
+
+                The models created with TAiL can be incorporated within other smart contract systems by referencing TAiL as an import, and interacting with the Predict function of the smart contract.
                 </p>
 
                 </div>
